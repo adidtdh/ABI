d5b5f1d7e2b23c9f19f2b5fc4ee0d70774e35c20
Adi Rathni <adirathni@gmail.com>
2023-12-18 15:57:55 -0500

    Added code

--- ABI.c
+++ ABI.c
@@ -1,3 +1,11 @@
+#include "AdiLinkedList.h"
+
 struct BigInteger{
-    
+    struct AdiLinkedList* data;
+};
+
+struct BigInteger* initBigInteger(){
+    struct BigInteger* bigint = (struct BigInteger*)malloc(sizeof(struct BigInteger));
+    bigint->data = initAdiLinkedList();
 }
+
